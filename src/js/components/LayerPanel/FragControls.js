1ece01a0a04db0d237a8e9c163f9a0f7be32dfa2
Kayla Kremer <kremer.kayla@gmail.com>
2019-06-10 15:32:31 -0400

    Update timeslider to use only one sliderValue, change Range to Slider

--- src/js/components/LayerPanel/FragControls.js
+++ src/js/components/LayerPanel/FragControls.js
@@ -8,6 +8,7 @@ import Slider from 'rc-slider';
 const createSliderWithTooltip = Slider.createSliderWithTooltip;
 const Range = createSliderWithTooltip(Slider.Range);
 
+
 const lossOptions = [];
 
 export default class FragControls extends Component {
@@ -22,7 +23,7 @@ export default class FragControls extends Component {
 
     this.state = {
       playing: false,
-      sliderValue: [],
+      sliderValue: 0,
       sliderMarks: {},
       holdSliderValueWhenPlaying: [],
       holdSliderMarksWhenPlaying: {}
@@ -30,7 +31,7 @@ export default class FragControls extends Component {
   }
 
   componentDidMount () {
-    const min = 1;
+    const min = 0;
     const max = 17;
     for ( let i = min; i <= max; i++ ) {
       lossOptions.push({ label: i < 10 ? `0${i} ` : `${i} `, value: i });
@@ -41,18 +42,18 @@ export default class FragControls extends Component {
 
     const sliderMarksObj = {};
     sliderMarkLabels.forEach((label, index) => {
-      sliderMarksObj[index + 1] = <small>{label}</small>;
+      sliderMarksObj[index] = <small>{label}</small>;
     });
 
     //- Update the defaults to be the last year
-    layerActions.updateLossTimeline.defer({
-      fromSelectedIndex: 0,
-      toSelectedIndex: 16
-    });
+    // layerActions.updateLossTimeline.defer({
+    //   fromSelectedIndex: 0,
+    //   toSelectedIndex: 16
+    // });
     //- Set the options in the store so others can use it
-    layerActions.setLossOptions.defer(lossOptions);
+    //layerActions.setLossOptions.defer(lossOptions);
     this.setState({
-      sliderValue: [lossOptions[0].value, lossOptions[lossOptions.length - 1].value],
+      //sliderValue: [lossOptions[0].value, lossOptions[lossOptions.length - 1].value],
       sliderMarks: sliderMarksObj
     });
   }
@@ -61,38 +62,34 @@ export default class FragControls extends Component {
     const { map } = this.context;
 
     if (map.getLayer && prevState.sliderValue !== this.state.sliderValue) {
-      this.updateDates(map.getLayer(layerKeys.FRAGMENTATION), this.state.sliderValue[0], this.state.sliderValue[1]);
+      this.updateDates(map.getLayer(layerKeys.FRAGMENTATION), this.state.sliderValue);
     }
 
-    if (this.props.lossFromSelectIndex !== this.state.sliderValue[0] - 1) {
-      this.setState({sliderValue: [this.props.lossFromSelectIndex + 1, this.props.lossToSelectIndex + 1]});
-    }
+    // if (this.props.lossFromSelectIndex !== this.state.sliderValue[0] - 1) {
+    //   this.setState({sliderValue: [this.props.lossFromSelectIndex + 1, this.props.lossToSelectIndex + 1]});
+    // }
 
-    if (this.props.lossToSelectIndex !== this.state.sliderValue[1] - 1) {
-      this.setState({sliderValue: [this.props.lossFromSelectIndex + 1, this.props.lossToSelectIndex + 1]});
-    }
+    // if (this.props.lossToSelectIndex !== this.state.sliderValue[1] - 1) {
+    //   this.setState({sliderValue: [this.props.lossFromSelectIndex + 1, this.props.lossToSelectIndex + 1]});
+    // }
 
-    const {canopyDensity, resetSlider} = this.props;
+    //const {resetSlider} = this.props;
     const {sliderValue} = this.state;
-    const fromYear = sliderValue[0];
-    const toYear = sliderValue[1];
+  
 
     if (map.loaded) {
 
       if (this.props.lossOptions.length) {
-        if (prevProps.canopyDensity !== canopyDensity) {
-          this.updateFragmentation(map.getLayer(layerKeys.FRAGMENTATION), sliderValue);
-        }
-        if (resetSlider) {
-          layerActions.shouldResetSlider(false);
-          this.updateDates(map.getLayer(layerKeys.FRAGMENTATION), lossOptions[0].label, lossOptions[lossOptions.length - 1].label);
-          this.setState({sliderValue: [lossOptions[0].value, lossOptions[lossOptions.length - 1].value]});
-        }
+        // if (resetSlider) {
+        //   layerActions.shouldResetSlider(false);
+        //   this.updateDates(map.getLayer(layerKeys.FRAGMENTATION), lossOptions[0].label, lossOptions[lossOptions.length - 1].label);
+        //   this.setState({sliderValue: [lossOptions[0].value, lossOptions[lossOptions.length - 1].value]});
+        // }
 
         if (prevContext.map !== map && Object.keys(prevContext.map).length !== 0) {
           const signal = map.on('update-end', () => {
             signal.remove();
-            this.updateDates(map.getLayer(layerKeys.TREE_COVER_LOSS), fromYear, toYear);
+            this.updateDates(map.getLayer(layerKeys.FRAGMENTATION), sliderValue);
           });
         }
       }
@@ -105,113 +102,128 @@ export default class FragControls extends Component {
     }
   }
 
-  updateDates (layer, fromYear, toYear) {
-    if (layer && layer.setDateRange) {
-      layer.setDateRange(fromYear, toYear);
+  updateDates (layer, sliderValue) {
+    // if (layer && layer.setDateRange) {
+    //   //layer.setDateRange(fromYear, toYear);
+    // }
+    let yearValue = sliderValue.toString();
+    if (yearValue.length === 1){
+     yearValue = `0${yearValue}`;
     }
-  }
-
-  updateFragmentation (layer, sliderValue) {
-    const { settings } = this.context;
-    const layerGroups = settings.layerPanel;
-    const layerConf = utils.getObject(layerGroups.GROUP_LCD.layers, 'id', this.props.layerId);
-    let baseUrl = layerConf.url;
-    console.log('baseUrl', baseUrl);
+    console.log('updating');
+    let baseUrl = layer.url;
     baseUrl = baseUrl.split('distance')[0] + 'distance_';
-    baseUrl += sliderValue[1];
+    baseUrl += yearValue;
     baseUrl += '/{level}/{col}/{row}.png';
     console.log('baseUrl', baseUrl);
-    layer.setUrl(baseUrl);
+    layer.url = baseUrl;
+    layer.refresh();
+    layer.hide();
+    layer.show();
   }
 
+  // updateFragmentation (layer, sliderValue) {
+  //   const { settings } = this.context;
+  //   const layerGroups = settings.layerPanel;
+  //   const layerConf = utils.getObject(layerGroups.GROUP_LCD.layers, 'id', this.props.layerId);
+  //   let baseUrl = layerConf.url;
+  //   console.log('baseUrl', baseUrl);
+  //   baseUrl = baseUrl.split('distance')[0] + 'distance_';
+  //   baseUrl += sliderValue[1];
+  //   baseUrl += '/{level}/{col}/{row}.png';
+  //   console.log('baseUrl', baseUrl);
+  //   layer.setUrl(baseUrl);
+  // }
+
   startVisualization = () => {
-    const { sliderValue, sliderMarks } = this.state;
-    const layer = this.context.map.getLayer(layerKeys.FRAGMENTATION);
-    const start = sliderValue[0];
-    let currentValue = start;
-    const stop = sliderValue[1];
+    // const { sliderValue, sliderMarks } = this.state;
+    // const layer = this.context.map.getLayer(layerKeys.FRAGMENTATION);
+    // const start = sliderValue[0];
+    // let currentValue = start;
+    // const stop = sliderValue[1];
 
-    const visualizeLoss = () => {
-      if (currentValue === stop + 1) {
-        currentValue = start;
-      }
+    // const visualizeLoss = () => {
+    //   if (currentValue === stop + 1) {
+    //     currentValue = start;
+    //   }
 
-      layer.setDateRange(start, currentValue);
-      layerActions.updateLossTimeline({
-        fromSelectedIndex: start,
-        toSelectedIndex: currentValue
-      });
-      const nextMark = currentValue % 2 === 0 ? currentValue + 1 : currentValue + 2;
-      const prevMark = currentValue % 2 === 0 ? currentValue - 1 : currentValue - 2;
-      // const shouldHideNextMark = nextMark <= lossOptions[lossOptions.length - 1].value;
-      // const shouldHidePrevMark = prevMark >= lossOptions[0].value;
+    //   layer.setDateRange(start, currentValue);
+    //   layerActions.updateLossTimeline({
+    //     fromSelectedIndex: start,
+    //     toSelectedIndex: currentValue
+    //   });
+    //   const nextMark = currentValue % 2 === 0 ? currentValue + 1 : currentValue + 2;
+    //   const prevMark = currentValue % 2 === 0 ? currentValue - 1 : currentValue - 2;
+    //   // const shouldHideNextMark = nextMark <= lossOptions[lossOptions.length - 1].value;
+    //   // const shouldHidePrevMark = prevMark >= lossOptions[0].value;
 
-      this.setState({
-        sliderValue: [start, currentValue],
-        sliderMarks: {
-          ...sliderMarks,
-          // ...(shouldHidePrevMark ? {[prevMark]: {
-          //   style: {
-          //     display: 'none'
-          //   }
-          // }} : {}),
-          [currentValue]: {
-            style: {
-              color: '#F0AB00'
-            },
-            label: <small>{lossOptions[currentValue - 1].label}</small>
-          },
-          // ...(shouldHideNextMark ? {[nextMark]: {
-          //   style: {
-          //     display: 'none'
-          //   }
-          // }} : {})
-        }
-      });
-      currentValue++;
-    };
+    //   this.setState({
+    //     sliderValue: [start, currentValue],
+    //     sliderMarks: {
+    //       ...sliderMarks,
+    //       // ...(shouldHidePrevMark ? {[prevMark]: {
+    //       //   style: {
+    //       //     display: 'none'
+    //       //   }
+    //       // }} : {}),
+    //       [currentValue]: {
+    //         style: {
+    //           color: '#F0AB00'
+    //         },
+    //         label: <small>{lossOptions[currentValue - 1].label}</small>
+    //       },
+    //       // ...(shouldHideNextMark ? {[nextMark]: {
+    //       //   style: {
+    //       //     display: 'none'
+    //       //   }
+    //       // }} : {})
+    //     }
+    //   });
+    //   currentValue++;
+    // };
 
-    this.timer = setInterval(visualizeLoss, 1000);
+    // this.timer = setInterval(visualizeLoss, 1000);
 
-    this.setState({
-      playing: true,
-      holdSliderValueWhenPlaying: sliderValue,
-      holdSliderMarksWhenPlaying: sliderMarks
-    });
+    // this.setState({
+    //   playing: true,
+    //   holdSliderValueWhenPlaying: sliderValue,
+    //   holdSliderMarksWhenPlaying: sliderMarks
+    // });
   }
 
   stopVisualization = () => {
-    const { holdSliderValueWhenPlaying, holdSliderMarksWhenPlaying } = this.state;
-    const fromYear = holdSliderValueWhenPlaying[0] - 1;
-    const toYear = holdSliderValueWhenPlaying[1] - 1;
+    // const { holdSliderValueWhenPlaying, holdSliderMarksWhenPlaying } = this.state;
+    // const fromYear = holdSliderValueWhenPlaying[0] - 1;
+    // const toYear = holdSliderValueWhenPlaying[1] - 1;
 
-    const layer = this.context.map.getLayer(layerKeys.FRAGMENTATION);
+    // const layer = this.context.map.getLayer(layerKeys.FRAGMENTATION);
 
-    clearInterval(this.timer);
-    layer.setDateRange(fromYear, toYear);
-    layerActions.updateLossTimeline({
-      fromSelectedIndex: fromYear,
-      toSelectedIndex: toYear
-    });
-    this.setState({
-      playing: false,
-      sliderValue: holdSliderValueWhenPlaying,
-      sliderMarks: holdSliderMarksWhenPlaying
-    });
+    // clearInterval(this.timer);
+    // layer.setDateRange(fromYear, toYear);
+    // layerActions.updateLossTimeline({
+    //   fromSelectedIndex: fromYear,
+    //   toSelectedIndex: toYear
+    // });
+    // this.setState({
+    //   playing: false,
+    //   sliderValue: holdSliderValueWhenPlaying,
+    //   sliderMarks: holdSliderMarksWhenPlaying
+    // });
   }
 
   handleSliderChange = sliderValue => {
+    console.log('slider value', sliderValue);
     this.setState({sliderValue});
 
-    layerActions.updateLossTimeline({
-      fromSelectedIndex: sliderValue[0] - 1,
-      toSelectedIndex: sliderValue[1] - 1
-    });
+    // layerActions.updateLossTimeline({
+    //   fromSelectedIndex: sliderValue[0] - 1,
+    //   toSelectedIndex: sliderValue[1] - 1
+    // });
   }
 
   render () {
     const {sliderValue, sliderMarks, playing} = this.state;
-    const disabled = sliderValue[0] === sliderValue[1];
+    const disabled = false;
     const disabledStyles = {
       opacity: '.5',
       color: '#aaa',
@@ -221,10 +233,10 @@ export default class FragControls extends Component {
     if (lossOptions.length === 0) {
       return <div className='timeline-container loss flex'>loading...</div>;
     }
-
+    console.log('slider value', sliderValue);
     return (
       <div className='timeline-container loss'>
-        <Range
+        <Slider
           min={lossOptions[0].value}
           max={lossOptions[lossOptions.length - 1].value}
           value={sliderValue}
